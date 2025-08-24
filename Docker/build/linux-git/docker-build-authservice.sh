26f8b8704f9e16a8a214c64838676d293b557199
Samantha Neubeck <2856339+Scassany@users.noreply.github.com>
2025-08-23 22:11:47 -0500

    Update docker-build-authservice.sh

--- Docker/build/linux-git/docker-build-authservice.sh
+++ Docker/build/linux-git/docker-build-authservice.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd ../../../
 docker build -t darkarchon/mare-synchronos-authservice:latest . -f Docker/build/Dockerfile-MareSynchronosAuthService-git --no-cache --pull --force-rm
-cd Docker/build/linux-local
+cd Docker/build/linux-git
