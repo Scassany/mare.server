4c3a85191ea222840a9218af1fa44959989db0dc
Samantha Neubeck <2856339+Scassany@users.noreply.github.com>
2025-08-23 22:10:05 -0500

    Update git linux build files

--- Docker/build/linux-git/docker-build-server.sh
+++ Docker/build/linux-git/docker-build-server.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker build -t darkarchon/mare-synchronos-server:latest . -f ../Dockerfile-MareSynchronosServer-git --no-cache --pull --force-rm
+cd ../../../
+docker build -t darkarchon/mare-synchronos-server:latest . -f Docker/build/Dockerfile-MareSynchronosServer-git --no-cache --pull --force-rm
+cd Docker/build/linux-local
