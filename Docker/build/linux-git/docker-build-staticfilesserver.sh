c9fd2b27dcfe2c0150333d8eeafb446e9d9d1aff
Samantha Neubeck <2856339+Scassany@users.noreply.github.com>
2025-08-23 22:11:32 -0500

    Update git linux build files

--- Docker/build/linux-git/docker-build-staticfilesserver.sh
+++ Docker/build/linux-git/docker-build-staticfilesserver.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker build -t darkarchon/mare-synchronos-staticfilesserver:latest . -f ../Dockerfile-MareSynchronosStaticFilesServer-git --no-cache --pull --force-rm
+cd ../../../
+docker build -t darkarchon/mare-synchronos-staticfilesserver:latest . -f Docker/build/Dockerfile-MareSynchronosStaticFilesServer-git --no-cache --pull --force-rm
+cd Docker/build/linux-git
